1ddf1cbf9423e2d9ee63097df70063b400e92a5e
Monica <contatomonicavaz@gmail.com>
2022-08-10 14:33:49 -0300

    fix search

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import {
-  BsGraphUp,
-  BsWallet2,
-  BsHourglassSplit,
   BsFillFileEarmarkTextFill,
+  BsGraphUp,
+  BsHourglassSplit,
+  BsWallet2,
 } from 'react-icons/bs';
+import { useParams } from 'react-router-dom';
 
 import MovieCard from '../Components/MovieCard';
 import './Movie.css';
@@ -15,66 +15,66 @@ const apiKey = import.meta.env.VITE_API_KEY;
 
 const Movies = () => {
   // Pegar o id que está na URL. Desestrutura.
-  const {id} = useParams();
+  const { id } = useParams();
   // Carregar o filme pela API
   const [movie, setMovie] = useState(null);
 
   // função que irá carregar o filme, ela espera uma url. Função asincrona.
-  const getMovie = async(url) => {
-    const res = await fetch (url);
+  const getMovie = async (url) => {
+    const res = await fetch(url);
     const data = await res.json();
     console.log(data);
     setMovie(data);
   };
-// formatar moeda
+  // formatar moeda
   const formatCurrency = (number) => {
-    return number.toLocaleString("en-US", {
-      style: "currency",
-      currency: "USD",
+    return number.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
     });
-  }
+  };
 
-  // Irá fazer a chamada 
+  // Irá fazer a chamada
   useEffect(() => {
     // Montar a URL que chama o filme de forma individual
-    const movieUrl = `${moviesURL}${id}?${apiKey}`;
+    const movieUrl = `${moviesURL}${id}?api_key=${apiKey}`;
     // Invocação da função
     getMovie(movieUrl);
   }, []);
 
   return (
-    <div className='movie-page'>
+    <div className="movie-page">
       {movie && (
-      <>
-        <MovieCard movie={movie} showLink={false} />
-        <p className="tagline">{movie.tagline}</p>
-        <div className="info">
-          <h3>
-            <BsWallet2 /> Orçamento:
-          </h3>
-          <p>{formatCurrency(movie.budget)}</p>
-        </div>
-        <div className="info">
-          <h3>
-            <BsGraphUp /> Receita:
-          </h3>
-          <p>{formatCurrency(movie.revenue)}</p>
-        </div>
-        <div className="info">
-          <h3>
-            <BsHourglassSplit /> Duração:
-          </h3>
-          <p>{movie.runtime} minutos</p>
-        </div>
-        <div className="info">
-          <h3>
-            <BsFillFileEarmarkTextFill /> Descrição:
-          </h3>
-          <p>{movie.overview}</p>
-        </div>
-      </>
-    )}
-  </div>
+        <>
+          <MovieCard movie={movie} showLink={false} />
+          <p className="tagline">{movie.tagline}</p>
+          <div className="info">
+            <h3>
+              <BsWallet2 /> Orçamento:
+            </h3>
+            <p>{formatCurrency(movie.budget)}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsGraphUp /> Receita:
+            </h3>
+            <p>{formatCurrency(movie.revenue)}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsHourglassSplit /> Duração:
+            </h3>
+            <p>{movie.runtime} minutos</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsFillFileEarmarkTextFill /> Descrição:
+            </h3>
+            <p>{movie.overview}</p>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
