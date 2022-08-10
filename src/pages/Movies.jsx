e1aed81df4cb4087544de310a26470c69ca63804
Carlos Augusto <95699480+Caarlos7x@users.noreply.github.com>
2022-08-09 21:31:33 -0300

    Add files via upload
    Projeto com React, consumindo a API do TMDB (filmes), utilizando hooks e React Router, criado com Vite.

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from 'react-icons/bs';
+
+import MovieCard from '../Components/MovieCard';
+import './Movie.css';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Movies = () => {
+  // Pegar o id que está na URL. Desestrutura.
+  const {id} = useParams();
+  // Carregar o filme pela API
+  const [movie, setMovie] = useState(null);
+
+  // função que irá carregar o filme, ela espera uma url. Função asincrona.
+  const getMovie = async(url) => {
+    const res = await fetch (url);
+    const data = await res.json();
+    console.log(data);
+    setMovie(data);
+  };
+// formatar moeda
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  }
+
+  // Irá fazer a chamada 
+  useEffect(() => {
+    // Montar a URL que chama o filme de forma individual
+    const movieUrl = `${moviesURL}${id}?${apiKey}`;
+    // Invocação da função
+    getMovie(movieUrl);
+  }, []);
+
+  return (
+    <div className='movie-page'>
+      {movie && (
+      <>
+        <MovieCard movie={movie} showLink={false} />
+        <p className="tagline">{movie.tagline}</p>
+        <div className="info">
+          <h3>
+            <BsWallet2 /> Orçamento:
+          </h3>
+          <p>{formatCurrency(movie.budget)}</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsGraphUp /> Receita:
+          </h3>
+          <p>{formatCurrency(movie.revenue)}</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsHourglassSplit /> Duração:
+          </h3>
+          <p>{movie.runtime} minutos</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsFillFileEarmarkTextFill /> Descrição:
+          </h3>
+          <p>{movie.overview}</p>
+        </div>
+      </>
+    )}
+  </div>
+  );
+};
+
+export default Movies;
